803d4bce6523b7a0e4235e0bcc0b90ffbe632e11
Valery Yazykov <valery.yazykov@interexy.com>
2024-06-11 11:00:31 +0300

    feature-4: add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'valery/short-links',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '42b5e9db922b4f55c6146255f92fabce5435cb87',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'valery/short-links' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '42b5e9db922b4f55c6146255f92fabce5435cb87',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
